413466e9c535788ec01f639ea578725714052050
jaycee <jaycee@jaycees-MacBook-Pro.local>
2015-12-27 14:08:57 +0800

    init

---
+++ Data.php
@@ -0,0 +1,8 @@
+<?php 
+$DatabaseType = 'mysql'; 
+$DatabaseServer = 'localhost'; 
+$DatabaseUsername = 'root'; 
+$DatabasePassword = 'root'; 
+$DatabaseName = 'fenghua'; 
+$DatabasePort = '3306'; 
+?>
